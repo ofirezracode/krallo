cdf8499492cda6f868e68c588d4fa7d6388f4880
Tamar Millgram <Tamarmillgram15@gmail.com>
2023-06-01 14:20:03 +0300

    UPDATE: Rendering home page and app header

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import { useDispatch, useSelector } from "react-redux";
 // import { CHANGE_COUNT } from "../store/user.reducer";
+import HomeImg from "../assets/img/home-img.webp"
 
 export default function Home() {
   //   const dispatch = useDispatch();
@@ -12,17 +13,16 @@ export default function Home() {
   //   }
 
   return (
-    <section className="home">
-      {/* <img src={logo} alt="Logo" style={{ maxWidth: '300px' }} />
-            <h2>
-                Count {count}
-                <button onClick={() => {
-                    changeCount(1)
-                }}>+</button>
-                <button onClick={() => {
-                    changeCount(10)
-                }}>+10</button>
-            </h2 > */}
+    <section className="home flex">
+            <div >
+              <h1> Karllo brings all your tasks, teammate, and tools together</h1>
+              <p>Keep everything in the same place-even if your team isn't.</p>
+              <form className="flex">
+              <input className="signup-input" type="email" id="email" name="email" />
+              <button className="signup-btn" >Sign up - it's free!</button>
+              </form>
+            </div>
+      <img src={HomeImg} alt="home-image" />
     </section>
   );
 }
