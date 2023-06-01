e40444c5bca9dc668293bde4a4b612ec6c129d4c
Tamar Millgram <Tamarmillgram15@gmail.com>
2023-06-01 14:56:33 +0300

    UPDATE: Adding design to home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -14,15 +14,17 @@ export default function Home() {
 
   return (
     <section className="home flex">
-            <div >
-              <h1> Karllo brings all your tasks, teammate, and tools together</h1>
+            <div className="home-txt">
+              <h1> Krallo brings all your tasks, teammate, and tools together</h1>
               <p>Keep everything in the same place-even if your team isn't.</p>
-              <form className="flex">
-              <input className="signup-input" type="email" id="email" name="email" />
+              <form className="flex wrap">
+              <input className="signup-input" type="email" id="email" name="email" placeholder="Email" />
               <button className="signup-btn" >Sign up - it's free!</button>
               </form>
             </div>
+      <div>
       <img src={HomeImg} alt="home-image" />
+      </div>
     </section>
   );
 }
