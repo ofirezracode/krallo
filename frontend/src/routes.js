abe56ca376b5844a9eb887524b3867e866ae5267
Tamar Millgram <Tamarmillgram15@gmail.com>
2023-06-02 16:24:55 +0300

    UPDATE: App header design - adding links Design

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import Home from './pages/home.jsx'
 import BoardIndex from './pages/board-index.jsx'
 import Workspaces from './pages/workspaces.jsx'
 import TaskDetails from './cmps/task-details.jsx'
+import LoginSignup from './pages/login-signup.jsx'
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
@@ -27,6 +28,12 @@ const routes = [
     component: <Workspaces />,
     label: 'Workspaces',
   },
+  {
+    path: '/login',
+    component: <LoginSignup />,
+    label: 'Login-Signup',
+  },
+
 ]
 
 export default routes
