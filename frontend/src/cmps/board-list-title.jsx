7cf453e863fc419410124e20096d95e436d74ddd
Etai Levi <etai3000@gmail.com>
2023-06-03 16:32:38 +0300

    FIX: Match convention to React

--- frontend/src/cmps/board-list-title.jsx
+++ frontend/src/cmps/board-list-title.jsx
@@ -2,9 +2,9 @@ import { BsPerson } from "react-icons/bs";
 
 export default function BoardListTitle({ title }) {
     return (
-        <div className="title flex center">
+        <header className="title flex center">
             <BsPerson className="title-icon" strokeWidth={'.5px'} />
             <h1>{title}</h1>
-        </div>
+        </header>
     )
 }
