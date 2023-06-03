30854611f3ac49a6532f2689dee84b7f889d06f1
Etai Levi <etai3000@gmail.com>
2023-06-03 16:31:56 +0300

    UPDATE: Small changes

--- frontend/src/cmps/board-list.jsx
+++ frontend/src/cmps/board-list.jsx
@@ -2,35 +2,35 @@ import BoardPreview from './board-preview'
 import BoardListTitle from './board-list-title'
 
 export default function BoardList({ boards, toggleIsStarred, isOnlyStarred, onAddBoard }) {
-  let filteredBoards = isOnlyStarred ? boards.filter((board) => board.isStarred) : boards
+    let filteredBoards = isOnlyStarred ? boards.filter((board) => board.isStarred) : boards
 
-  return (
-    <section className="board-list-container">
-      {isOnlyStarred ? <BoardListTitle title={'Starred boards'} /> : <BoardListTitle title={'Your boards'} />}
-      <ul className="board-list clean-list flex wrap">
-        {filteredBoards.map((board) => {
-          let boardStyle = {}
-          if (board.style) {
-            if (board.style.type === 'bgColor') {
-              boardStyle = { backgroundColor: board.style.bgColor }
-            } else if (board.style.type === 'img') {
-              boardStyle = { backgroundImage: `url(${board.style.imgUrl})` }
-            }
-          }
-          return (
-            <li key={board._id} style={boardStyle} className="board-preview">
-              <BoardPreview board={board} toggleIsStarred={toggleIsStarred} />
-            </li>
-          )
-        })}
-        {!isOnlyStarred && (
-          <li className="new-board" onClick={onAddBoard}>
-            <article className="flex align-center justify-center">
-              <p>Create new board</p>
-            </article>
-          </li>
-        )}
-      </ul>
-    </section>
-  )
+    return (
+        <section className="board-list-container">
+            {isOnlyStarred ? <BoardListTitle title={'Starred boards'} /> : <BoardListTitle title={'Your boards'} />}
+            <ul className="board-list clean-list flex wrap">
+                {filteredBoards.map((board) => {
+                    let boardStyle = {}
+                    if (board.style) {
+                        if (board.style.type === 'bgColor') {
+                            boardStyle = { backgroundColor: board.style.bgColor }
+                        } else if (board.style.type === 'img') {
+                            boardStyle = { background: `url(${board.style.imgUrl}) center center / cover` }
+                        }
+                    }
+                    return (
+                        <li key={board._id} style={boardStyle} className="board-preview">
+                            <BoardPreview board={board} toggleIsStarred={toggleIsStarred} />
+                        </li>
+                    )
+                })}
+                {!isOnlyStarred && (
+                    <li className="new-board" onClick={onAddBoard}>
+                        <article className="flex align-center justify-center">
+                            <p>Create new board</p>
+                        </article>
+                    </li>
+                )}
+            </ul>
+        </section>
+    )
 }
--- frontend/src/cmps/board-preview.jsx
+++ frontend/src/cmps/board-preview.jsx
@@ -3,16 +3,16 @@ import { BsStar, BsStarFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
 
 export default function BoardPreview({ board, toggleIsStarred }) {
-  return (
-    <article>
-      <Link to={`/board/${board._id}`}>
-        <h4>{board.title}</h4>
-        {board.isStarred ? (
-          <BsStarFill className="star-fill" onClick={(ev) => toggleIsStarred(ev, board)} />
-        ) : (
-          <BsStar className="star-empty" strokeWidth={'1px'} onClick={(ev) => toggleIsStarred(ev, board)} />
-        )}
-      </Link>
-    </article>
-  )
+    return (
+        <article>
+            <Link to={`/board/${board._id}`}>
+                <h4>{board.title}</h4>
+                {board.isStarred ? (
+                    <BsStarFill className="star-fill" onClick={(ev) => toggleIsStarred(ev, board)} />
+                ) : (
+                    <BsStar className="star-empty" strokeWidth={'1px'} onClick={(ev) => toggleIsStarred(ev, board)} />
+                )}
+            </Link>
+        </article>
+    )
 }
