a7e726922f8aedd38d1d38e2fbfc1fcd347413ba
ofirezracode <ofirezracode@gmail.com>
2023-06-03 13:22:23 +0300

    UPDATE: refactored board-list and board-preview

--- frontend/src/cmps/board-preview.jsx
+++ frontend/src/cmps/board-preview.jsx
@@ -2,36 +2,17 @@ import React from 'react'
 import { BsStar, BsStarFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
 
-export default function BoardPreview({ boards, toggleIsStarred, isOnlyStarred, onAddBoard }) {
-    return (
-        <ul className="board-list clean-list flex wrap">
-            {boards.map(board => {
-                let boardStyle = {}
-                if (board.style) {
-                    if (board.style.type === 'bgColor') {
-                        boardStyle = { backgroundColor: board.style.bgColor }
-                    } else if (board.style.type === 'img') {
-                        boardStyle = { backgroundImage: `url(${board.style.imgUrl})` }
-                    }
-                }
-                return <li key={board._id} style={boardStyle} className='board-preview'>
-                    <article>
-                        <Link to={`/board/${board._id}`}>
-                            <h4>{board.title}</h4>
-                            {board.isStarred ? <BsStarFill className="star-fill" onClick={(ev) => toggleIsStarred(ev, board)} /> :
-                                <BsStar className="star-empty" strokeWidth={'1px'} onClick={(ev) => toggleIsStarred(ev, board)} />}
-                        </Link>
-                    </article>
-                </li>
-            })
-            }
-            {!isOnlyStarred && <li className="new-board" onClick={onAddBoard}>
-                {/* <Link to={`/board/${board._id}`}> */}
-                <article className="flex align-center justify-center">
-                    <p>Create new board</p>
-                </article>
-                {/* </Link> */}
-            </li>}
-        </ul>
-    )
+export default function BoardPreview({ board, toggleIsStarred }) {
+  return (
+    <article>
+      <Link to={`/board/${board._id}`}>
+        <h4>{board.title}</h4>
+        {board.isStarred ? (
+          <BsStarFill className="star-fill" onClick={(ev) => toggleIsStarred(ev, board)} />
+        ) : (
+          <BsStar className="star-empty" strokeWidth={'1px'} onClick={(ev) => toggleIsStarred(ev, board)} />
+        )}
+      </Link>
+    </article>
+  )
 }
