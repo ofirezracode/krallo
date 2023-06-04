60782d31d4bc2b79963d39c03ba00092b4a80fd4
Etai Levi <etai3000@gmail.com>
2023-06-05 00:15:22 +0300

    UPDATE: Alignment settings were changed

--- frontend/src/cmps/board-list-title.jsx
+++ frontend/src/cmps/board-list-title.jsx
@@ -2,7 +2,7 @@ import { BsPerson } from 'react-icons/bs'
 
 export function BoardListTitle({ title }) {
   return (
-    <header className="board-title-preview flex center">
+    <header className="board-title-preview flex align-center">
       <BsPerson className="title-icon" strokeWidth={'.5px'} />
       <h2>{title}</h2>
     </header>
