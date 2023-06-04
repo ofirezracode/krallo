3c26beaaa9fd71d8e68bb0ed23368fe3d0b6aca5
Etai Levi <etai3000@gmail.com>
2023-06-04 09:55:42 +0300

    UPDATE: Added backdrop to task-details

--- frontend/src/cmps/task-details.jsx
+++ frontend/src/cmps/task-details.jsx
@@ -13,75 +13,79 @@ function TaskDetails() {
   // const task = boardService.getTaskById(taskId, boardId)
 
   return (
-    <article className="task-details">
-      <div className="cover-color">
-        <div className="cover-btn-container">
-          <button>
-            {/* <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style="transform: rotate(0.75turn) translateY(-20%) translateX(22%);"><path d="M8 15V1h6a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H8zm6 1a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12z"></path></svg> */}
-            <p>Cover</p>
-          </button>
+    <section className="screen">
+      <div className="backdrop"></div>
+
+      <article className="task-details">
+        <div className="cover-color">
+          <div className="cover-btn-container">
+            <button>
+              {/* <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style="transform: rotate(0.75turn) translateY(-20%) translateX(22%);"><path d="M8 15V1h6a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H8zm6 1a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12z"></path></svg> */}
+              <p>Cover</p>
+            </button>
+          </div>
         </div>
-      </div>
-      <header className="flex">
-        <div className="title-img">
-          <BsFillCreditCardFill className="card-title-img" />
-        </div>
-        <div className="task-title">
-          <h1>Drag and Drop</h1>
-          <p>in list <Link>Code Review</Link></p>
-        </div>
-      </header>
-      <section className="task-details-container">
-        <section className="card-details-container">
+        <header className="flex">
+          <div className="title-img">
+            <BsFillCreditCardFill className="card-title-img" />
+          </div>
+          <div className="task-title">
+            <h1>Drag and Drop</h1>
+            <p>in list <Link>Code Review</Link></p>
+          </div>
+        </header>
+        <section className="task-details-container">
+          <section className="card-details-container">
 
 
-          <section className="members-labels">
-            <div className="members-wrapper">
-              <h5>Members</h5>
-              <div className='members'>
-                < img src={Ofir} className='member-img' alt="" />
-                < img src={Etai} className='member-img' alt="" />
-                < img src={Tamar} className='member-img' alt="" />
-                <button className="add-member"><BsPlusLg /></button>
+            <section className="members-labels">
+              <div className="members-wrapper">
+                <h5>Members</h5>
+                <div className='members'>
+                  < img src={Ofir} className='member-img' alt="" />
+                  < img src={Etai} className='member-img' alt="" />
+                  < img src={Tamar} className='member-img' alt="" />
+                  <button className="add-member"><BsPlusLg /></button>
+                </div>
               </div>
-            </div>
-            <div className="labels-wrapper">
-              <h5>Labels</h5>
-              <div className="labels">
-                <button className="label-btn">Logic</button>
-                <button className="add-label"><BsPlusLg /></button>
+              <div className="labels-wrapper">
+                <h5>Labels</h5>
+                <div className="labels">
+                  <button className="label-btn">Logic</button>
+                  <button className="add-label"><BsPlusLg /></button>
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-        </section>
-        <section className="add-to-card-container">
-          <h5>Add to card</h5>
-          <section className="add-to-card-btns">
-            <button className="add-btn flex justify-center">
-              <BsPerson className="add-to-card-img" />
-              <p>Members</p>
-            </button>
-            <button className="add-btn flex justify-center">
-              <BsTag className="add-to-card-img" />
-              <p>Labels</p>
-            </button>
-            <button className="add-btn flex justify-center">
-              <BsCheck2Square className="add-to-card-img" />
-              <p>Checklist</p>
-            </button>
-            <button className="add-btn flex justify-center">
-              <BsClock />
-              <p>Dates</p>
-            </button>
-            <button className="add-btn flex justify-center">
-              <BsPaperclip className="add-to-card-img" />
-              <p>Attachment</p>
-            </button>
+          </section>
+          <section className="add-to-card-container">
+            <h5>Add to card</h5>
+            <section className="add-to-card-btns">
+              <button className="add-btn flex justify-center">
+                <BsPerson className="add-to-card-img" />
+                <p>Members</p>
+              </button>
+              <button className="add-btn flex justify-center">
+                <BsTag className="add-to-card-img" />
+                <p>Labels</p>
+              </button>
+              <button className="add-btn flex justify-center">
+                <BsCheck2Square className="add-to-card-img" />
+                <p>Checklist</p>
+              </button>
+              <button className="add-btn flex justify-center">
+                <BsClock />
+                <p>Dates</p>
+              </button>
+              <button className="add-btn flex justify-center">
+                <BsPaperclip className="add-to-card-img" />
+                <p>Attachment</p>
+              </button>
+            </section>
           </section>
         </section>
-      </section>
-    </article>
+      </article>
+    </section>
   );
 }
 
