9f7a5f67732049b24927dad4615d83bce4f5b8cb
ofirezracode <ofirezracode@gmail.com>
2023-06-07 15:14:01 +0300

    FEATURE: clicking on date in task preview changes its status

--- frontend/src/cmps/task-preview-indicators/indicator-due-date.jsx
+++ frontend/src/cmps/task-preview-indicators/indicator-due-date.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from 'react'
-import { BsClock, BsSquare, BsStop } from 'react-icons/bs'
+import React, { useEffect, useState } from 'react'
+import { BsClock, BsStop, BsCheck2Square } from 'react-icons/bs'
 import { utilService } from '../../services/util.service'
 
-export function IndicatorDueDate({ dueDate }) {
+export function IndicatorDueDate({ dueDate, onDateClick }) {
   const [isHovered, setIsHovered] = useState(false)
+  const [timestamp, setTimestamp] = useState(dueDate.timestamp)
+  const [isCompleted, setIsCompleted] = useState(dueDate.isCompleted)
+
+  useEffect(() => {
+    setTimestamp(dueDate.timestamp)
+    setIsCompleted(dueDate.isCompleted)
+  }, [dueDate])
+
   let indicatorClass
-  if (dueDate.isCompleted) {
+  if (isCompleted) {
     indicatorClass = 'complete'
-  } else if (utilService.hasTimestampPassed(dueDate.timestamp)) {
+  } else if (utilService.hasTimestampPassed(timestamp)) {
     indicatorClass = 'overdue'
   }
+
+  function onClicked(e) {
+    setIsCompleted((prev) => !prev)
+    onDateClick(e)
+  }
+
   return (
     <li className={`indicator-due-date ${indicatorClass}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-      <span className="flex">
-        {isHovered && <BsStop className="indicator-icon square" />}
+      <span onClick={onClicked} className="flex">
+        {isHovered && !isCompleted && <BsStop className="indicator-icon square" />}
+        {isHovered && isCompleted && <BsCheck2Square className="indicator-icon square-checked" />}
         {!isHovered && <BsClock className="indicator-icon clock" />}
 
-        <label>{utilService.formatDate(dueDate.timestamp)}</label>
+        <label>{utilService.formatDate(timestamp)}</label>
       </span>
     </li>
   )
