e97fd8049aa356006a6c2169e65c041efaa0f035
Etai Levi <etai3000@gmail.com>
2023-06-07 10:58:04 +0300

    UPDATE: Changed path of filter-icon in boardHeader

--- frontend/src/cmps/board-header.jsx
+++ frontend/src/cmps/board-header.jsx
@@ -4,6 +4,7 @@ import { updateBoard } from '../store/board.actions'
 import { Popover } from './popover'
 import { usePopover } from '../customHooks/usePopover'
 import { Loader } from './loader'
+import FilterIcon from '../assets/img/svg/filter-icon.svg'
 
 export function BoardHeader({ board }) {
   const [title, setTitle] = useState(board ? board.title : '')
@@ -65,15 +66,16 @@ export function BoardHeader({ board }) {
           )}
         </li>
         <li className="flex align-center">
-          <button title="Filter cards flex align-center">
-            <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <button title="Filter cards" className='flex align-center'>
+            {/* <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M4.61799 6C3.87461 6 3.39111 6.78231 3.72356 7.44721L3.99996 8H20L20.2763 7.44721C20.6088 6.78231 20.1253 6 19.3819 6H4.61799ZM10.8618 17.7236C10.9465 17.893 11.1196 18 11.309 18H12.6909C12.8803 18 13.0535 17.893 13.1382 17.7236L14 16H9.99996L10.8618 17.7236ZM17 13H6.99996L5.99996 11H18L17 13Z"
                 fill="currentColor"
               ></path>
-            </svg>
+            </svg> */}
+            <img src={FilterIcon} className='filter-icon' alt="filter-icon" />
             <p>Filter</p>
           </button>
           <span>|</span>
