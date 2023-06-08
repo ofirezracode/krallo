a191dca60ac998e56e6bff7d58c347bcf2b41cc6
Tamar Millgram <Tamarmillgram15@gmail.com>
2023-06-08 23:09:53 +0300

    UPDATE: Todos crud is done

--- frontend/src/cmps/popovers/popover-delete-checklist.jsx
+++ frontend/src/cmps/popovers/popover-delete-checklist.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { PopoverCmpHeader } from './popover-cmp-header'
 
-export function PopverDeleteChecklist({ checklist, onDeleteChecklist, onClose }) {
+export function PopoverDeleteChecklist({ checklist, onDeleteChecklist, onClose }) {
+
+    function onClickDelete() {
+        onDeleteChecklist(checklist._id)
+        onClose()
+    }
+
     return (
         <section className='delete-checklist'>
             <PopoverCmpHeader title="Delete Checklist?" onClose={onClose} />
             <p className='delete-checklist-pra' >Deleting a checklist is permanent and there is no way to get it back.</p>
-            <button className='btn delete-btn' onClick={() => onDeleteChecklist(checklist._id)}>Delete checklist</button>
+            <button className='btn delete-btn' onClick={() => onClickDelete()}>Delete checklist</button>
         </section>
     )
 }
