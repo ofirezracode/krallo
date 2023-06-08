85f928151fb26b547ed546d7b4434f52d6a78b9f
Etai Levi <etai3000@gmail.com>
2023-06-08 23:25:03 +0300

    FEATURE: Added board menu

--- frontend/src/cmps/board-menu.jsx
+++ frontend/src/cmps/board-menu.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { HiXMark } from 'react-icons/hi2'
 
-export function BoardMenu() {
+export function BoardMenu({ board, setIsMenuHidden, showMenuClass }) {
+
+  let boardStyle = {}
+  if (board.style) {
+    if (board.style.type === 'bgColor') {
+      boardStyle = { backgroundColor: board.style.bgColor }
+    } else if (board.style.type === 'img') {
+      boardStyle = { background: `url(${board.style.imgUrl}) center center / cover` }
+    }
+  }
   return (
-    <section className="board-menu">
-      <div></div>
+    <section className={`board-menu ${showMenuClass}`}>
+      <section className="menu-header flex center">
+        <h3>Menu</h3>
+        <button title="Close the board menu" onClick={() => setIsMenuHidden(prevIsMenuHidden => !prevIsMenuHidden)}><HiXMark /></button>
+      </section>
+      <section className="menu-content">
+        <button className="change-background flex">
+          <div className="board-bg-img" style={boardStyle}></div>
+          <h4>Change background</h4>
+        </button>
+      </section>
     </section>
-  );
+  )
 }
