998e8bd87ea56e660e244541be65c976669ab686
Etai Levi <etai3000@gmail.com>
2023-06-09 09:23:19 +0300

    UPDATE: Board menu design

--- frontend/src/cmps/board-menu.jsx
+++ frontend/src/cmps/board-menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiXMark } from 'react-icons/hi2'
+import KralloIcon from '../assets/img/svg/krallo-icon.svg'
 
 export function BoardMenu({ board, setIsMenuHidden, showMenuClass }) {
 
@@ -15,14 +16,23 @@ export function BoardMenu({ board, setIsMenuHidden, showMenuClass }) {
     <section className={`board-menu ${showMenuClass}`}>
       <section className="menu-header flex center">
         <h3>Menu</h3>
-        <button title="Close the board menu" onClick={() => setIsMenuHidden(prevIsMenuHidden => !prevIsMenuHidden)}><HiXMark /></button>
+        <button
+          title="Close the board menu" onClick={() => setIsMenuHidden(prevIsMenuHidden => !prevIsMenuHidden)}>
+          <HiXMark />
+        </button>
       </section>
       <section className="menu-content">
+        <button>
+          <h4>About this board</h4>
+          <img src={KralloIcon} alt="krallo-icon" />
+        </button>
         <button className="change-background flex">
           <div className="board-bg-img" style={boardStyle}></div>
           <h4>Change background</h4>
         </button>
+
       </section>
+
     </section>
   )
 }
