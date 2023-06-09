f26ebbe4ff53946044354a2849dfc8f7eba5965f
Etai Levi <etai3000@gmail.com>
2023-06-09 17:23:39 +0300

    UPDATE: Created cmps for board menu

---
+++ frontend/src/cmps/board-menu/menu-about.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function MenuAbout(board) {
+    return (
+        <div>MenuAbout</div>
+    )
+}
---
+++ frontend/src/cmps/board-menu/menu-activities-list.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function MenuActivitiesList() {
+    return (
+        <div>MenuActivitiesList</div>
+    )
+}
