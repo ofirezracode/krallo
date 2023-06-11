1fafd6724a022775d83a14769c034ea7d8769acd
ofirezracode <ofirezracode@gmail.com>
2023-06-11 15:20:27 +0300

    FIX: activitis will not be injected to board object

--- frontend/src/services/board.service.js
+++ frontend/src/services/board.service.js
@@ -65,9 +65,9 @@ async function saveTask(board, updatedTask, activity) {
   task = { ...updatedTask }
   group.tasks[taskIdx] = task
 
-  if (activity) {
-    board.activities.unshift(activity)
-  }
+  // if (activity) {
+  //   board.activities.unshift(activity)
+  // }
 
   const newBoard = await save(board)
   return newBoard
@@ -83,9 +83,9 @@ async function saveNewTask(board, groupId, updatedTask, activity) {
 
   group.tasks.push(updatedTask)
 
-  if (activity) {
-    board.activities.unshift(activity)
-  }
+  // if (activity) {
+  //   board.activities.unshift(activity)
+  // }
 
   const newBoard = await save(board)
   return newBoard
