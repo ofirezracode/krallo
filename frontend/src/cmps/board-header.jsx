c4ee4b3c830bc6df31b55e888c378da226aa82d6
Etai Levi <etai3000@gmail.com>
2023-07-26 15:50:35 +0300

    UPDATE: Removed comment lines

--- frontend/src/cmps/board-header.jsx
+++ frontend/src/cmps/board-header.jsx
@@ -96,8 +96,7 @@ export function BoardHeader({ onChangeTitle, showMenuClass, setIsMenuHidden }) {
   let shownMembers = []
   if (members?.length) {
     if (window.innerWidth < 500) {
-      shownMembers.push(<img key={members[0]._id} className="member-img" src={members[0].imgUrl} alt="" onClick={(e) => onOpenPopover(e, { member: members[0] }, 'member-info')} />)
-      // shownMembers.push(<img key={members[0]._id} className="member-img" src={members[0].imgUrl} alt="" />)
+      shownMembers.push(<img key={members[0]._id} className="member-img" src={members[0].imgUrl} alt={members[0].fullname} onClick={(e) => onOpenPopover(e, { member: members[0] }, 'member-info')} />)
       shownMembers.push(
         <div className="mobile-more-members flex center">
           <label>+{members.length - 1}</label>
@@ -121,8 +120,7 @@ export function BoardHeader({ onChangeTitle, showMenuClass, setIsMenuHidden }) {
                 type="text"
                 value={title}
                 onChange={handleChange}
-                // style={{ width: `${inputWidth}px` }} // Set the width dynamically
-                style={{ width: `${title.length * 10.8}px` }} // Set the width dynamically
+                style={{ width: `${title.length * 10.8}px` }}
                 onFocus={handleFocus}
               />
             </form>
@@ -142,20 +140,11 @@ export function BoardHeader({ onChangeTitle, showMenuClass, setIsMenuHidden }) {
             className="flex align-center"
             onClick={(e) => onOpenPopover(e, { onFilterBy: () => { }, widthOverride: '384px' }, 'filter')}
           >
-            {/* <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4.61799 6C3.87461 6 3.39111 6.78231 3.72356 7.44721L3.99996 8H20L20.2763 7.44721C20.6088 6.78231 20.1253 6 19.3819 6H4.61799ZM10.8618 17.7236C10.9465 17.893 11.1196 18 11.309 18H12.6909C12.8803 18 13.0535 17.893 13.1382 17.7236L14 16H9.99996L10.8618 17.7236ZM17 13H6.99996L5.99996 11H18L17 13Z"
-                fill="currentColor"
-              ></path>
-            </svg> */}
             <img src={FilterIcon} className="filter-icon" alt="filter-icon" />
             <p>Filter</p>
           </button>
           <span>|</span>
           <div className="members">
-            {/* {members?.length && members.map((member) => <img key={member._id} className="member-img" src={member.imgUrl} alt="" />)} */}
             {shownMembers}
           </div>
           <button
