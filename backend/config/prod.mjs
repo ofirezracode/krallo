66225d69a74e5391120f253b8aebc1780c099291
Etai Levi <etai3000@gmail.com>
2023-09-04 15:14:20 +0300

    Changed mongoDB json sources to etai's collections

--- backend/config/prod.mjs
+++ backend/config/prod.mjs
@@ -1,5 +1,5 @@
 export default {
-  dbURL: 'mongodb+srv://ofirezracode:spBMwdAnkB0KrryC@cluster-krallo.r27h5el.mongodb.net/',
-  // dbURL: 'mongodb+srv://etai3000:313297483Etailevi@krallo.zezhvbo.mongodb.net/',
+  // dbURL: 'mongodb+srv://ofirezracode:spBMwdAnkB0KrryC@cluster-krallo.r27h5el.mongodb.net/',
+  dbURL: 'mongodb+srv://etai3000:313297483Etailevi@krallo.zezhvbo.mongodb.net/',
   dbName: 'krallo',
 }
